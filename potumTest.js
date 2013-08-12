f9c4ac0ba5cb22a25f0a991ef480ba52c82def2c
Bargavi Chinni Krishnamoorthy <ChinnaChinni@dyn-166-203.wireless.concordia.ca>
2013-08-11 20:54:09 -0400

    final test code js

--- potumTest.js
+++ potumTest.js
@@ -24,12 +24,6 @@ potumTest.prototype.testFactNull = function() {
 	assertEquals(expected, actual);
 };
 
-potumTest.prototype.testFactUndefined = function() {
-	var actual = fact(undefined);
-	var expected = 1;
-	assertEquals(expected, actual);
-};
-
 potumTest.prototype.testFactString = function() {
 	var actual = fact("One");
 	var expected = 1;
@@ -42,110 +36,128 @@ potumTest.prototype.testPi = function() {
 	assertEquals(expected, actual);
 };
 
-potumTest.prototype.testPowA = function() {
+potumTest.prototype.testPowPositive = function() {
 	var result = pow(2,3);
-	var expected = 8;
+	var expected = Math.pow(2,3);
 	assertEquals(expected, result);
 };
 
-potumTest.prototype.testPowB = function() {
+potumTest.prototype.testPowNullFirst = function() {
 	var result = pow(null,3);
-	var expected = 0;
+	var expected = Math.pow(null,3);
 	assertEquals(expected, result);
 };
 
-potumTest.prototype.testPowC = function() {
+potumTest.prototype.testPowBothNull = function() {
 	var result = pow(null,null);
-	var expected = 1;
+	var expected = Math.pow(null,null);
 	assertEquals(expected, result);
 };
 
-potumTest.prototype.testPowD = function() {
-	var result = pow(-2.0,null);
-	var expected = 1;
+potumTest.prototype.testPowNullLast = function() {
+	var result = pow(-2.2,null);
+	var expected = Math.pow(-2.2, null);
 	assertEquals(expected, result);
 };
 
-potumTest.prototype.testPowE = function() {
-	var result = pow(-2.0,null);
-	var expected = 1;
+potumTest.prototype.testPowString = function() {
+	var result = pow("hi","hi");
+	var expected = Math.pow("hi","hi");
 	assertEquals(expected, result);
 };
 
-potumTest.prototype.testPowF = function() {
-	var result = pow(2.5,-9.9);
-	var expected = 1;
+potumTest.prototype.testPowNegative = function() {
+	var result = pow(-2,-2);
+	var expected = Math.pow(-2,-2);
 	assertEquals(expected, result);
 };
 
-potumTest.prototype.testPowG = function() {
+potumTest.prototype.testPowBaseNegative = function() {
+	var result = pow(-2,2);
+	var expected = Math.pow(-2,2);
+	assertEquals(expected, result);
+};
+potumTest.prototype.testPowDouble = function() {
 	var result = pow(2.5,2.3);
-	var expected = 15.625;
+	var expected = Math.pow(2.5,2.3);
 	assertEquals(expected, result);
 };
 
-potumTest.prototype.testPowG = function() {
-	var result = pow("hi","hello");
-	var expected = 1;
-	assertEquals(expected, result);
-};
-
-potumTest.prototype.testLSinA = function() {
-	var x = -2;
-	var result = lsin(x) + x;
-	var expected = Math.sin(x);
-	assertEquals(expected, result);
-};
-
-potumTest.prototype.testLSinB = function() {
+potumTest.prototype.testLSinPostive = function() {
 	var x = 2;
 	var result = lsin(x) + x;
 	var expected = Math.sin(x);
 	assertEquals(expected, result);
 };
 
-potumTest.prototype.testLSinC = function() {
+potumTest.prototype.testLSinZero = function() {
+	var x = 0;
+	var result = lsin(x) + x;
+	var expected = lsin(x) + x;
+	assertEquals(expected, result);
+};
+
+potumTest.prototype.testLSinNull = function() {
 	var x = null;
 	var result = lsin(x) + x;
 	var expected = Math.sin(x);
 	assertEquals(expected, result);
 };
 
-potumTest.prototype.testLSinD = function() {
+potumTest.prototype.testLSinString = function() {
 	var x = "String";
 	var result = lsin(x) + x;
 	var expected = "NaNString";
 	assertEquals(expected, result);
 };
 
-potumTest.prototype.testLCosA = function() {
-	var x = -1;
-	var result = lcos(x) + 1;
-	var expected = Math.cos(x);
+potumTest.prototype.testLSinDouble = function() {
+	var x = 1.1;
+	var result = lsin(x) + x;
+	var expected = Math.sin(x);
 	assertEquals(expected, result);
 };
 
-potumTest.prototype.testLCosB = function() {
+potumTest.prototype.testLSinNegative = function() {
+	var x = -1;
+	var result = lsin(x) + x;
+	var expected = Math.sin(x);
+	assertEquals(expected, result);
+};
+potumTest.prototype.testLCosPostive = function() {
 	var x = 1;
 	var result = lcos(x) + 1;
 	var expected = Math.cos(x);
 	assertEquals(expected, result);
 };
 
-potumTest.prototype.testLCosC = function() {
+potumTest.prototype.testLCosZero = function() {
+	var x = 0;
+	var result = lcos(x) + 1;
+	var expected = Math.cos(x);
+	assertEquals(expected, result);
+};
+
+potumTest.prototype.testLCosNull = function() {
 	var x = null;
 	var result = lcos(x) + 1;
 	var expected = Math.cos(x);
 	assertEquals(expected, result);
 };
 
-potumTest.prototype.testLCosD = function() {
-	var x = "String";
+potumTest.prototype.testLCosDouble = function() {
+	var x = 3.3;
 	var result = lcos(x) + 1;
 	assertNotNull(result);
 	assertNotUndefined(result);
 };
 
+potumTest.prototype.testLCosNegative = function() {
+	var x = -3;
+	var result = lcos(x) + 1;
+	assertNotNull(result);
+	assertNotUndefined(result);
+};
 potumTest.prototype.testSolve = function() {
 	var a = solve(f, 0, 2 * pi);
 	var expected = 2.3098814600100575;
